8b746a86812d3df122375d541af3ae2da727b380
adindaop <adindaoctadia@gmail.com>
2015-09-02 11:38:52 +0700

    Revert "pindahfile"
    This reverts commit a74dc05d66259c83d746ffca788539b710d6598f.

--- latihan1.py
+++
@@ -1,4 +0,0 @@
-print "Selamat datang"
-nama = raw_input ("masukkan nama anda :")
-umur = input ("masukkan umur anda :")
-print "Halo nama saya {0}, umur saya {1} tahun".format(nama, umur)
--- latihan2-luassegitiga.py
+++
@@ -1,4 +0,0 @@
-print "Menghitung luas segitiga"
-alas = input("masukkan alas :")
-tinggi = input("masukkan tinggi :")
-print "luas segitiga adalah {0}".format(alas*tinggi/2)
